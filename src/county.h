ba42e240619cc5791a6d24a2be3c72b8086d0926
Samantha Bennett <samanthabennett@ufl.edu>
2023-11-22 18:44:32 -0500

    Change parameters of County objects

--- src/county.h
+++ src/county.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <map>
+#include <county.hpp>
 
 using std::string;
 using std::vector;
@@ -20,5 +22,9 @@ struct County {
 	bool signal;
 	bool day;
 
-	County(string county, string state, int severity, double visibility, string weather, bool crossing, bool junction, bool station, bool stop, bool signal, bool day);
+	County(string county, string state, int severity, vector<double> visibility, map<string, int> weather, int crossing, int junction, int station, int stop, int signal, int day, int night);
+	//int = count the number of crashes that are present in crossings, junctions, stations, stops, signals
+	//day or night == amount of crashes that happen during the day/night
+	//vector a visibility to get to average for each county 
+	//map of weather = counts the amount of each weather pattern present during each crash 
 };
