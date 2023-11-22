d11e5fb37555662e4caba3f6a9ae4c5f7fa31ca6
GrossTrevor <grosstrevor000@gmail.com>
2023-11-22 11:10:01 -0500

    main

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+	return 0;
+}
