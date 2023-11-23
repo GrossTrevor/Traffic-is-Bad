31cf45e6a643f029f162f7fed1b001ee5cbf7d4a
Samantha Bennett <samanthabennett@ufl.edu>
2023-11-22 19:46:16 -0500

    Make code that removes unwanted columns/data from the csv file (makes the file more managable)

--- src/county.h
+++ src/county.h
@@ -17,12 +17,12 @@ struct County {
 	map<string, int> weather;
 	int crossing;
 	int junction;
-	int station;
 	int stop;
 	int signal;
 	int day;
+	int night;
 
-	County(string county, string state, int severity, vector<double> visibility, map<string, int> weather, int crossing, int junction, int station, int stop, int signal, int day, int night);
+	County(string county, string state, int severity, vector<double> visibility, map<string, int> weather, int crossing, int junction, int stop, int signal, int day, int night);
 	//int = count the number of crashes that are present in crossings, junctions, stations, stops, signals
 	//day or night == amount of crashes that happen during the day/night
 	//vector a visibility to get to average for each county 
