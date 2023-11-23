d7d8b96071c994a193fb603ef78e063b36c3a2d5
Samantha Bennett <117133024+samnbe@users.noreply.github.com>
2023-11-23 14:50:32 -0500

    fixed unadded function

--- src/county.h
+++ src/county.h
@@ -27,14 +27,13 @@ struct County {
 	County();
 	void ChangeWeather(string w_state);
 	void AddVisibility(string visible);
+	void AddDayOrNight(string time);
 	void AddCrossing(string line);
 	void AddJunction(string line);
 	void AddStop(string line);
 	void AddSignal(string line);
 	void AddSeverity(string s);
 	void AddCountyAndState(string county, string state);
-
-	//County(string county, string state, int severity, vector<double> visibility, map<string, int> weather, int crossing, int junction, int stop, int signal, int day, int night);
 
 	//int = count the number of crashes that are present in crossings, junctions, stations, stops, signals
 	//day or night == amount of crashes that happen during the day/night
