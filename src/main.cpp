0f6e2e5e24a46d5d4b6d39ddba2fa260a1120352
Samantha Bennett <samanthabennett@ufl.edu>
2023-11-29 00:53:55 -0500

    edit main

--- src/main.cpp
+++ src/main.cpp
@@ -24,18 +24,23 @@ int main() {
 	ifstream infile("US_Accidents_2022_Data.csv"); 
 	string line = "";
 	string state = "";
+	string severity = "";
+	string county = "";
 
 
 	//right now, making obj for every crash -> not wanted, no way of knowing county is already created 
 	while (!infile.eof()) {
-		//search in structures here, then add or edit the county 
 		County c_obj;
 
-		getline(infile, line, ','); 
-		c_obj.AddSeverity(line);  
-		getline(infile, line, ',');  
+		getline(infile, severity, ','); 
+		c_obj.AddSeverity(severity);  
+		getline(infile, county, ',');  
 		getline(infile, state, ',');  
-		c_obj.AddCountyAndState(line, state); 
+		c_obj.AddCountyAndState(county, state); 
+
+		//search through data structures here, edit or add to county 
+		//use state and county strings to search 
+
 		getline(infile, line, ',');
 		c_obj.AddVisibility(line); 
 		getline(infile, line, ',');
