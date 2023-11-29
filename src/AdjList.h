55731a512eeea37d44e406a5d95e9b6730f41a95
Samantha Bennett <samanthabennett@ufl.edu>
2023-11-28 21:11:22 -0500

    make skeleton for adjlist

--- src/AdjList.h
+++ src/AdjList.h
@@ -1,2 +1,24 @@
 #pragma once
+#include <map>
+#include <vector>
+#include <iomanip> 
+#include <algorithm> 
+#include "county.h"
 
+using std::map;
+using std::vector; 
+using std::string;
+
+
+class AdjList {
+	map<string, vector<County>> states_to_county;
+public:
+	AdjList();
+	~AdjList();
+
+	void AddState();  //add state to the list 
+	void AddCounty(); //add county to a state 
+
+	bool CheckState();  //check if state already exists 
+	bool CheckCounty(); //check if county already exists 
+};
--- src/AdjList.hpp
+++ src/AdjList.hpp
@@ -1,1 +1,12 @@
+#include "AdjList.h"
+
+AdjList::AdjList() {
+	//create empty map
+	state_to_county = map<string, vector<County>>();
+}
+
+AdjList::~AdjList() {
+	//empty map 
+	state_to_county.clear();
+}
 
