a7f5073af52f7ad2190f01943a33e26f5986dc3a
GrossTrevor <grosstrevor000@gmail.com>
2023-12-04 16:20:24 -0500

    heap out working

--- src/main.cpp
+++ src/main.cpp
@@ -152,6 +152,15 @@ int main() {
 	County county_obj;
 	State state_obj;
 	vector<pair<string, string>> state_county;
+	bool isFirst = true;
+	bool addXtra = false;
+
+	getline(county_data, line2);
+	county_data2 << line2 << endl;
+	getline(county_data, line2);
+	county_data2 << line2 << endl;
+	getline(county_data, line2);
+	county_data2 << line2 << endl;
 
 	while (!heap.EmptyHeap()) {
 		state_obj = heap.ExtractMax();
@@ -160,20 +169,40 @@ int main() {
 				getline(county_data, line2);
 				if (line2.length() > 14 && line2.substr(9, 6) == "COUNTY") {
 					county = line2.substr(19, line2.length() - 1 - 20);
-					tempout = line2 + '\n';
+					tempout += line2 + '\n';
 				}
 				if (line2.length() > 14 && line2.substr(9, 5) == "STATE") {
 					state = line2.substr(18, line2.length() - 1 - 19);
-					tempout = line2 + '\n';
+					tempout += line2 + '\n';
 					if ((state == state_obj.GetName()) && !(heap.SearchCounty(county, state))) {
 						county_obj = state_obj.FindCounty(county);
+						if (isFirst) {
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+							isFirst = false;
+						}
+						if (state == "CA" && county == "San Francisco") {
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+						}
+						if (state == "IL" && county == "Pulaski")
+							addXtra = true;
 						county_data2 << tempout;
-						county_data2 << "        \"SEVERITY\": \"" << to_string(county_obj.GetAvgSeverity()) << "\"," << endl;
+						tempout = "";
+						if(to_string(county_obj.GetAvgSeverity()) == "nan")
+							county_data2 << "        \"SEVERITY\": \"0\"," << endl;
+						else
+							county_data2 << "        \"SEVERITY\": \"" << to_string(county_obj.GetAvgSeverity()) << "\"," << endl;
 						county_data2 << "        \"CROSSING\": \"" << to_string(county_obj.GetCrossing()) << "\"," << endl;
 						county_data2 << "        \"DAY\": \"" << to_string(county_obj.GetDay()) << "\"," << endl;
 						county_data2 << "        \"JUNCTION\": \"" << to_string(county_obj.GetJunction()) << "\"," << endl;
 						county_data2 << "        \"NIGHT\": \"" << to_string(county_obj.GetNight()) << "\"," << endl;
-						county_data2 << "        \"POOR_VISIBILITY\": \"" << to_string(county_obj.GetAvgVisibility()) << "\"," << endl;
+						if (to_string(county_obj.GetAvgVisibility()) == "nan")
+							county_data2 << "        \"POOR_VISIBILITY\": \"0\"," << endl;
+						else
+							county_data2 << "        \"POOR_VISIBILITY\": \"" << to_string(county_obj.GetAvgVisibility()) << "\"," << endl;
 						county_data2 << "        \"SIGNAL\": \"" << to_string(county_obj.GetSignal()) << "\"," << endl;
 						county_data2 << "        \"STOP\": \"" << to_string(county_obj.GetStop()) << "\"," << endl;
 						county_data2 << "        \"WFAIR\": \"" << to_string(county_obj.GetWeather("Fair")) << "\"," << endl;
@@ -181,6 +210,11 @@ int main() {
 						county_data2 << "        \"WFOG\": \"" << to_string(county_obj.GetWeather("Fog")) << "\"," << endl;
 						county_data2 << "        \"WRAIN\": \"" << to_string(county_obj.GetWeather("Rain")) << "\"," << endl;
 						county_data2 << "        \"WSNOW\": \"" << to_string(county_obj.GetWeather("Snow")) << "\"" << endl;
+
+						if (addXtra) {
+							county_data2 << "      },\n      \"type\": \"Feature\"\n    },\n";
+							addXtra = false;
+						}
 
 						getline(county_data, line2);
 						getline(county_data, line2);
@@ -198,6 +232,14 @@ int main() {
 					}
 					else if ((state == state_obj.GetName()) || ((find(state_county.begin(), state_county.end(), make_pair(county, state)) == state_county.end()) && ((state == "AK") || (state == "HI") || (state == "PR")))) {
 						state_county.push_back(make_pair(county, state));
+						if (isFirst) {
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+							tempout.erase(0, tempout.find('\n') + 1);
+							isFirst = false;
+						}
+						county_data2 << tempout;
+						tempout = "";
 						county_data2 << "        \"SEVERITY\": \"0\"," << endl;
 						county_data2 << "        \"CROSSING\": \"0\"," << endl;
 						county_data2 << "        \"DAY\": \"0\"," << endl;
@@ -226,14 +268,17 @@ int main() {
 						getline(county_data, line2);
 						getline(county_data, line2);
 					}
+					tempout = "";
 				}
 				else if (line2.length() < 18 || (line2.length() > 14 && !(line2.substr(9, 6) == "COUNTY" || line2.substr(9, 8) == "SEVERITY" || line2.substr(9, 8) == "CROSSING" || line2.substr(9, 3) == "DAY" || line2.substr(9, 8) == "JUNCTION" || line2.substr(9, 5) == "NIGHT" || line2.substr(9, 15) == "POOR_VISIBILITY" || line2.substr(9, 6) == "SIGNAL" || line2.substr(9, 4) == "STOP" || line2.substr(9, 5) == "WFAIR" || line2.substr(9, 7) == "WCLOUDY" || line2.substr(9, 4) == "WFOG" || line2.substr(9, 5) == "WRAIN" || line2.substr(9, 5) == "WSNOW")))
-					county_data2 << line2 << endl;
+					tempout += line2 + '\n';
 			}
 		}
+		tempout = "";
 		county_data.clear();
 		county_data.seekg(0);
 	}
+	county_data2 << "      },\n		\"type\": \"Feature\"\n    }\n  ],\n};\n";
 
 	return 0;
 }
